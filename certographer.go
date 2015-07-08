a1d9140cd75d841ba0f2e2979225f7d16101ccdf
Alex Swehla <alex.swehla@gmail.com>
2015-07-07 22:00:05 -0700

    changed license to MIT

--- certographer.go
+++ certographer.go
@@ -1,20 +1,3 @@
-/*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, see <http://www.gnu.org/licenses/>.
-*
-* Copyright (C) Alex Swehla,
- */
-
 // Package certographer provides methods for generating a Certificate Authority and managing SSL certificates
 package ca
 
@@ -151,24 +134,3 @@ func newCertificate() (*x509.Certificate, error) {
 	}
 	return &certificate, nil
 }
-
-/*
-func GenerateCA(keyType string, bitDepth int, expiration int, subject pkix.Name) (interface{}, *x509.Certificate, error) {
-
-	switch keyType {
-	case "ecdsa":
-		privateKey, err := ecdsa.GenerateKey(rand.Reader, bitDepth)
-		break
-	case "rsa":
-		privateKey, err := rsa.GenerateKey(rand.Reader, bitDepth)
-		break
-	default:
-		return nil, nil, errors.New("Key type not supported!")
-	}
-
-	template := newCertificate()
-	template.Subject = subject
-
-	return privateKey, certificate, nil
-}
-*/
